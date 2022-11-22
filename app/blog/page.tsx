90515c1951d298e64f40692786f08cbf2629765a
Meth Munindradasa <methsndr@gmail.com>
2022-11-22 14:50:11 +0530

    Add proper alignment

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,7 @@ import StyledHeader from "../../components/styled-header";
 
 export default function Blog() {
   return (
-    <div className="flex w-full h-screen justify-center items-center">
+    <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
       <StyledHeader gradient={true} text="Coming Soon!" />
     </div>
   );
