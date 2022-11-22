aa98025858c00690e3fe8dd77c0c0c226105d8ca
Meth Munindradasa <methsndr@gmail.com>
2022-11-22 22:58:54 +0530

    Add socials to mobile navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -38,13 +38,13 @@ function generateLinks(pages: string[], pathname: any, isMobile: boolean) {
   return links;
 }
 
-function socials() {
+function socials(isMobile: boolean) {
   return (
-    <div className="flex">
+    <div className={isMobile ? "flex mt-4" : "flex"}>
       <Link href="https://www.linkedin.com/in/methm/">
         <AiOutlineLinkedin className="text-2xl text-white" />
       </Link>
-      <div className="w-8" />
+      <div className={isMobile ? "w-4" : "w-8"} />
       <Link href="https://github.com/muffledMitosis">
         <AiFillGithub className="text-2xl text-white" />
       </Link>
@@ -61,7 +61,7 @@ export default function Navbar() {
       <div className="hidden md:visible md:flex justify-center my-10">
         <div className="flex w-2/3 justify-evenly">
           {generateLinks(pages, pathname, false)}
-          {socials()}
+          {socials(false)}
         </div>
       </div>
       <GiHamburgerMenu className={(!closed ? "hidden" : "visible") + " text-3xl absolute text-white md:hidden visible right-1 top-1"} onClick={() => setClosed(!closed)} />
@@ -70,6 +70,7 @@ export default function Navbar() {
         <div className="mt-2 ml-4">
           <div className="flex flex-col bg-dark-background mt-8">
             {generateLinks(pages, pathname, true)}
+            {socials(true)}
           </div>
         </div>
       </div>
