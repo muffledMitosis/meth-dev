8d1f61cdd1061032b2e664003c955626d3d0d5e2
Meth Munindradasa <methsndr@gmail.com>
2022-11-22 14:36:23 +0530

    Proper centering of header

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,20 @@ export default function Home() {
   return (
     <div>
       <main>
-        <div className="flex w-full h-screen flex-row justify-center items-center">
-          <div>
-            <div className="core-color-gradient">
-              <StyledHeader gradient={false} text="Meth" />
-              <StyledHeader gradient={false} text="Munindradasa" />
+        {/* <div className="flex w-full h-screen flex-row justify-center items-center"> */}
+        <div className="absolute left-1/2 transform translate-y-full -translate-x-1/2">
+          <div className="flex flex-row items-center justify-center w-full">
+            <div>
+              <div className="core-color-gradient">
+                <StyledHeader gradient={false} text="Meth" />
+                <StyledHeader gradient={false} text="Munindradasa" />
+              </div>
+              <div className="text-white font-bold">Full-stack Developer</div>
+              <div className="text-not-selected">Passionate about tech</div>
             </div>
-            <div className="text-white font-bold">Full-stack Developer</div>
-            <div className="text-not-selected">Passionate about tech</div>
+            <div className="w-32"></div>
+            <ProfileIcon imageIcon={PFP} />
           </div>
-          <div className="w-32"></div>
-          <ProfileIcon imageIcon={PFP} />
         </div>
       </main>
 
