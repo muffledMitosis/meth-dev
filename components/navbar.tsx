e07470079579de70a80f65f06ea0e920e6bb1b76
Meth Munindradasa <methsndr@gmail.com>
2022-11-22 15:24:47 +0530

    Improve readability

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,42 +12,53 @@ const pages: string[] = [
   'Dashboard',
 ];
 
+function generateLinks(pages: string[], pathname: any) {
+  let links = pages.map(page => {
+
+    const route = page == base ? "/" : page.toLowerCase();
+    const color =
+      (pathname == "/" + page.toLowerCase() ||
+        (page == base && pathname == "/"))
+        ? "text-white" : "text-not-selected";
+
+    const elem = (
+      <Link
+        className={color + ""}
+        href={route}
+        key={page}>
+        {page}
+      </Link>
+    );
+
+    return elem;
+  });
+
+  return links;
+}
+
+function socials() {
+  return (
+    <div className="flex">
+      <Link href="https://www.linkedin.com/in/methm/">
+        <AiOutlineLinkedin className="text-2xl text-white" />
+      </Link>
+      <div className="w-8" />
+      <Link href="https://github.com/muffledMitosis">
+        <AiFillGithub className="text-2xl text-white" />
+      </Link>
+    </div>
+  );
+}
+
 export default function Navbar() {
   const pathname = usePathname();
 
   return (
     <div className="flex justify-center my-10">
       <div className="flex w-2/3 justify-evenly">
-        {pages.map(page => {
-
-          const route = page == base ? "/" : page.toLowerCase();
-          const color =
-            (pathname == "/" + page.toLowerCase() ||
-              (page == base && pathname == "/"))
-              ? "text-white" : "text-not-selected";
-
-          const elem = (
-            <Link
-              className={color + ""}
-              href={route}
-              key={page}>
-              {page}
-            </Link>
-          );
-
-          return elem;
-        })}
-        <div className="flex">
-          <Link href="https://www.linkedin.com/in/methm/">
-            <AiOutlineLinkedin className="text-2xl text-white" />
-          </Link>
-          <div className="w-8" />
-          <Link href="https://github.com/muffledMitosis">
-            <AiFillGithub className="text-2xl text-white" />
-          </Link>
-        </div>
+        {generateLinks(pages, pathname)}
+        {socials()}
       </div>
-
     </div>
   );
 }
